4dc63f724bd785a352822999a749e119399001c8
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-09-13 13:16:36 +0200

    added app.js. trying functions

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function clicCoral() {
+    var c = document.getElementsByClassName("clicColores");
+    var i;
+    for (i = 0; i < c.length; i++) {
+    c[i].classList.toggle("coral");
+    }
+}
+
+function clicAmarillo() {
+    var a = document.getElementsByClassName("clicColores");
+    var i;
+    for (i = 0; i < a.length; i++) {
+    a[i].classList.toggle("amarillo");
+    }
+}
+
+function clicGris() {
+    var c = document.getElementsByClassName("clicColores");
+    var i;
+    for (i = 0; i < c.length; i++) {
+    c[i].classList.toggle("gris");
+    }
+}
+
+function clicTurquesa() {
+    var t = document.getElementsByClassName("clicColores");
+    var i;
+    for (i = 0; i < t.length; i++) {
+    t[i].classList.toggle("turquesa");
+    }
+}
